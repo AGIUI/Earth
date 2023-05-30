6f0bfae6d4252aa45afc15cd7829f1affe4fb431
EOMZON <116791118+EOMZON@users.noreply.github.com>
2023-05-30 17:14:34 +0800

    20230530 LanguageDetector

--- src/locales/i18nConfig.ts
+++ src/locales/i18nConfig.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./en.json";
 import zh from "./zh.json";
@@ -13,11 +14,15 @@ const resources = {
   },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng:"en", //// 默认语言
-  fallbackLng: "en", // 如果找不到当前语言的翻译文本，将使用该语言作为回退
-  interpolation: {
-    escapeValue: false, // 不需要对翻译文本进行转义
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en", // 如果找不到当前语言的翻译文本，将使用该语言作为回退
+    lng: navigator.language,
+    debug: true,
+    interpolation: {
+      escapeValue: false, // 不需要对翻译文本进行转义
+    },
+  });
