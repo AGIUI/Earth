8a5be86ef60ff5c776788714287427a02d93b937
shadowcz007 <chizhiwei007@163.com>
2023-06-12 20:47:44 +0800

    合并

---
+++ src/components/agent/base.tsx
@@ -0,0 +1,29 @@
+
+
+function addHighlight(dom: any) {
+    if (dom) dom.style.border = "1px solid yellow"
+}
+
+
+async function inputByQueryBase(query: string, text: string) {
+    const inp: any = document.querySelector(query);
+    inp.innerText = text;
+    inp.value = text;
+    inp.focus()
+    inp.select()
+    document.execCommand("insertText", false, text)
+    addHighlight(inp)
+}
+
+function clickByQueryBase(query: string) {
+    const dom: any = document.querySelector(query);
+    if (dom) {
+        dom.click();
+        addHighlight(dom)
+    }
+}
+
+export {
+    inputByQueryBase,
+    clickByQueryBase
+}
