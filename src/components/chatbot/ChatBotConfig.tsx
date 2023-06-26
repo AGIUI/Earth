60590413aa580912cc08898e2c90398b80cd25ff
shadowcz007 <chizhiwei007@163.com>
2023-06-26 11:34:55 +0800

    -

--- src/components/chatbot/ChatBotConfig.tsx
+++ src/components/chatbot/ChatBotConfig.tsx
@@ -232,6 +232,6 @@ function createTalkData(type: string, json: any) {
     return data
 }
 
-export default {
+export {
     get, createTalkData, getOutput, getInput, getAgentOpts, getTranslate, getRoleOpts, createRoleOpts
 }
